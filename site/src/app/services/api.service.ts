8931bf8286afc71eebc5b834f09f65bec2599425
Nelsutro <nelsonerices28@gmail.com>
2025-08-29 19:31:51 -0400

    conexion entre back y front

---
+++ site/src/app/services/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = 'http://127.0.0.1:8000/api';
+
+  constructor(private http: HttpClient) {}
+
+  getTest() {
+    return this.http.get(`${this.baseUrl}/test`);
+  }
+}
