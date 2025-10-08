a719d42073f02e573c5f6f16085c7adf5fd414dc
Nelsutro <nelsonerices28@gmail.com>
2025-10-08 16:35:45 -0300

    test

--- site/src/environments/environment.cert.ts
+++ site/src/environments/environment.cert.ts
@@ -1,7 +1,6 @@
 export const environment = {
 	production: false,
 	apiUrl: 'https://api-cert.factufast.com/api',
-	appUrl: 'https://app-cert.factufast.com',
 	appName: 'FactuFast (Cert)',
 	version: '1.0.0'
 };
